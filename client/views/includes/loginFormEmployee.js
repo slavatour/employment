984a08872506fd78cbef9d702b55728da80f2b98
Home <dzvinok.cv@gmail.com>
2014-12-18 00:18:32 +0200

    first commit

--- client/views/includes/loginFormEmployee.js
+++ client/views/includes/loginFormEmployee.js
@@ -4,13 +4,11 @@ Template.loginFormEmployee.helpers({
 Template.loginFormEmployee.events({
 	"click #registerEmployeeOpen": function(e, target) {
 		e.preventDefault();
-		$("#employeeForm").hide();
-		$(".employeeNewForm").show();
+		showRegisterForm();
 	},
 	"click #signInEmloyeeOpen": function(e, target) {
 		e.preventDefault();
-		$("#employeeForm").show();
-		$(".employeeNewForm").hide();
+		showLogInForm();
 	},
 	"submit #register-form": function(e, t) {
 		e.preventDefault();
@@ -20,9 +18,31 @@ Template.loginFormEmployee.events({
 			if(err) {
 				console.log(err);
 			} else {
-				
+				showLogInForm();		
 			}
 		});
 		return false;
+	},
+	"submit #employeeForm": function(e,t) {
+		e.preventDefault();
+		var email = t.find('#inputEmail').value,
+        	password = t.find('#inputPassword').value;
+    	Meteor.loginWithPassword(email, password, function(err){
+    		if(err) {
+				console.log("no");
+    		} else {
+				console.log("yes");
+    		}
+    	});
+    	return false;
 	}
 });
+
+function showRegisterForm() {
+	$("#employeeForm").hide();
+	$(".employeeNewForm").show();	
+}
+function showLogInForm() {
+	$("#employeeForm").show();
+	$(".employeeNewForm").hide();
+}
